c177d5b30e3c8f79cb2bfe298f8c1bf64b40fdc3
Claudio Scheer <claudioscheer@protonmail.com>
2021-06-07 20:11:25 -0300

    Use Docker multi-stage build

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
+FROM claudioscheer/build-neovim-development AS neovim
+WORKDIR /
+
 FROM texlive/texlive:latest
+WORKDIR /home
 
 ENV TZ=UTC
 ENV TERM=xterm-256color
 ENV LANG=C.UTF-8
+
+RUN mkdir /usr/nvim
+COPY --from=neovim /nvim/ /usr/nvim/
 
 COPY requirements.sh /requirements.sh
 RUN sh /requirements.sh
@@ -10,4 +17,3 @@ RUN sh /requirements.sh
 ENV NVIDIA_VISIBLE_DEVICES all
 ENV NVIDIA_DRIVER_CAPABILITIES graphics,utility,compute
 
-WORKDIR /home
--- requirements.sh
+++ requirements.sh
@@ -21,19 +21,6 @@ apt-get install -y -qq --no-install-recommends \
     mesa-utils \
     glmark2
 
-# Install Neovim from source. I should use Docker multi-stage.
-apt-get install -y ninja-build \
-    gettext \
-    libtool \
-    libtool-bin \
-    autoconf \
-    automake \
-    cmake \
-    g++ \
-    pkg-config \
-    unzip
-git clone https://github.com/neovim/neovim /neovim
-cd /neovim && make CMAKE_BUILD_TYPE=Release && make CMAKE_INSTALL_PREFIX=/usr/nvim install
 ln -s /usr/nvim/bin/nvim /usr/bin/vim
 
 curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
